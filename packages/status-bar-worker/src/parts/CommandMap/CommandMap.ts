0d19c5d78805fc8a589a68f50ddf3af88302e59b
Le Vivilet <levivilet@pm.me>
2025-12-08 16:21:27 +0100

    feature: update commandMap imports (#26)

--- packages/status-bar-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/status-bar-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,13 +1,17 @@
 import { terminate } from '@lvce-editor/viewlet-registry'
 import * as StatusBar from '../Create/Create.ts'
-import { handleClick, itemLeftUpdate, itemRightCreate, itemRightUpdate, loadContent } from '../LoadContent/LoadContent.ts'
+import * as HandleClick from '../HandleClick/HandleClick.ts'
+import * as ItemLeftUpdate from '../ItemLeftUpdate/ItemLeftUpdate.ts'
+import * as ItemRightCreate from '../ItemRightCreate/ItemRightCreate.ts'
+import * as ItemRightUpdate from '../ItemRightUpdate/ItemRightUpdate.ts'
+import * as LoadContent from '../LoadContent/LoadContent.ts'
 
 export const commandMap = {
   'StatusBar.create': StatusBar.create,
-  'StatusBar.handleClick': handleClick,
-  'StatusBar.itemLeftUpdate': itemLeftUpdate,
-  'StatusBar.itemRightCreate': itemRightCreate,
-  'StatusBar.itemRightUpdate': itemRightUpdate,
-  'StatusBar.loadContent': loadContent,
+  'StatusBar.handleClick': HandleClick.handleClick,
+  'StatusBar.itemLeftUpdate': ItemLeftUpdate.itemLeftUpdate,
+  'StatusBar.itemRightCreate': ItemRightCreate.itemRightCreate,
+  'StatusBar.itemRightUpdate': ItemRightUpdate.itemRightUpdate,
+  'StatusBar.loadContent': LoadContent.loadContent,
   'StatusBar.terminate': terminate,
 }
