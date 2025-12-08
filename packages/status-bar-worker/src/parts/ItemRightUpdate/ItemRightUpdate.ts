c20556e95819ba86aa5b70afc3971cab6eb735ff
Le Vivilet <levivilet@pm.me>
2025-12-08 16:22:19 +0100

    feature: move updateArray (#27)

--- packages/status-bar-worker/src/parts/ItemRightUpdate/ItemRightUpdate.ts
+++ packages/status-bar-worker/src/parts/ItemRightUpdate/ItemRightUpdate.ts
@@ -1,25 +1,10 @@
 import type { StatusBarItem } from '../StatusBarItem/StatusBarItem.ts'
 import type * as StatusBarState from '../StatusBarState/StatusBarState.ts'
-
-const updateArray = (items: readonly StatusBarItem[], newItem: Readonly<StatusBarItem>): StatusBarItem[] => {
-  const index = getIndex(items, newItem)
-  const before = items.slice(0, index)
-  const after = items.slice(index + 1)
-  return [...before, newItem, ...after]
-}
-
-const getIndex = (items: readonly StatusBarItem[], item: Readonly<StatusBarItem>): number => {
-  for (let i = 0; i < items.length; i++) {
-    if (items[i].name === item.name) {
-      return i
-    }
-  }
-  return -1
-}
+import * as UpdateArray from '../UpdateArray/UpdateArray.ts'
 
 export const itemRightUpdate = (state: Readonly<StatusBarState.StatusBarState>, newItem: Readonly<StatusBarItem>): StatusBarState.StatusBarState => {
   const { statusBarItemsRight } = state
-  const newStatusBarItemsRight = updateArray([...statusBarItemsRight], newItem)
+  const newStatusBarItemsRight = UpdateArray.updateArray([...statusBarItemsRight], newItem)
   return {
     ...state,
     statusBarItemsRight: newStatusBarItemsRight,
---
+++ packages/status-bar-worker/src/parts/UpdateArray/UpdateArray.ts
@@ -0,0 +1,17 @@
+import type { StatusBarItem } from '../StatusBarItem/StatusBarItem.ts'
+
+const getIndex = (items: readonly StatusBarItem[], item: Readonly<StatusBarItem>): number => {
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].name === item.name) {
+      return i
+    }
+  }
+  return -1
+}
+
+export const updateArray = (items: readonly StatusBarItem[], newItem: Readonly<StatusBarItem>): StatusBarItem[] => {
+  const index = getIndex(items, newItem)
+  const before = items.slice(0, index)
+  const after = items.slice(index + 1)
+  return [...before, newItem, ...after]
+}
