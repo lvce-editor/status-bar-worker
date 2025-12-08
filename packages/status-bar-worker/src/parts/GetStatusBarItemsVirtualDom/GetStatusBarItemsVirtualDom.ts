15bc24f55a2b990459f8689670fdc062d8f5da87
Le Vivilet <levivilet@pm.me>
2025-12-08 16:24:00 +0100

    tsc

--- packages/status-bar-worker/src/parts/GetStatusBarItemsVirtualDom/GetStatusBarItemsVirtualDom.ts
+++ packages/status-bar-worker/src/parts/GetStatusBarItemsVirtualDom/GetStatusBarItemsVirtualDom.ts
@@ -1,30 +1,8 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
-<<<<<<< HEAD
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
-=======
-import { VirtualDomElements, AriaRoles, text } from '@lvce-editor/virtual-dom-worker'
->>>>>>> origin/main
 import type { StatusBarItem } from '../StatusBarItem/StatusBarItem.ts'
 import * as GetStatusBarItemVirtualDom from '../GetStatusBarItemVirtualDom/GetStatusBarItemVirtualDom.ts'
 
-<<<<<<< HEAD
-=======
-const getStatusBarItemVirtualDom = (statusBarItem: StatusBarItem): readonly VirtualDomNode[] => {
-  const { tooltip } = statusBarItem
-  return [
-    {
-      childCount: 1,
-      className: ClassNames.StatusBarItem,
-      role: AriaRoles.Button,
-      tabIndex: -1,
-      title: tooltip,
-      type: VirtualDomElements.Div,
-    },
-    text(statusBarItem.text),
-  ]
-}
-
->>>>>>> origin/main
 export const getStatusBarItemsVirtualDom = (items: readonly StatusBarItem[], className: string): readonly VirtualDomNode[] => {
   return [
     {
