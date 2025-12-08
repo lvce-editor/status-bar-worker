a3d780c0cfe7efd186f869f40a86bfe92767de36
Le Vivilet <levivilet@pm.me>
2025-12-08 18:02:45 +0100

    feature: add initialize

--- packages/status-bar-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/status-bar-worker/src/parts/CommandMap/CommandMap.ts
@@ -2,6 +2,7 @@ import { terminate } from '@lvce-editor/viewlet-registry'
 import * as StatusBar from '../Create/Create.ts'
 import { diff2 } from '../Diff2/Diff2.ts'
 import * as HandleClick from '../HandleClick/HandleClick.ts'
+import { initialize } from '../Initialize/Initialize.ts'
 import * as ItemLeftUpdate from '../ItemLeftUpdate/ItemLeftUpdate.ts'
 import * as ItemRightCreate from '../ItemRightCreate/ItemRightCreate.ts'
 import * as ItemRightUpdate from '../ItemRightUpdate/ItemRightUpdate.ts'
@@ -16,6 +17,7 @@ export const commandMap = {
   'StatusBar.create': StatusBar.create,
   'StatusBar.diff2': diff2,
   'StatusBar.handleClick': wrapCommand(HandleClick.handleClick),
+  'StatusBar.initialize': initialize,
   'StatusBar.itemLeftUpdate': wrapCommand(ItemLeftUpdate.itemLeftUpdate),
   'StatusBar.itemRightCreate': wrapCommand(ItemRightCreate.itemRightCreate),
   'StatusBar.itemRightUpdate': wrapCommand(ItemRightUpdate.itemRightUpdate),
