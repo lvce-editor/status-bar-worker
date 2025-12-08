99a1dbcba0bf33050d42578cf4a651bbd305de9a
Le Vivilet <levivilet@pm.me>
2025-12-08 16:03:01 +0100

    feature: add preference type (#18)

--- packages/status-bar-worker/src/parts/StatusBarPreferences/StatusBarPreferences.ts
+++ packages/status-bar-worker/src/parts/StatusBarPreferences/StatusBarPreferences.ts
@@ -1,6 +1,6 @@
 import * as Preferences from '../Preferences/Preferences.ts'
 
-export const itemsVisible = (): boolean => {
-  const statusBarItemsPreference = Preferences.get('statusBar.itemsVisible') ?? false
+export const itemsVisible = async (): Promise<boolean> => {
+  const statusBarItemsPreference = (await Preferences.get('statusBar.itemsVisible')) ?? false
   return statusBarItemsPreference
 }
