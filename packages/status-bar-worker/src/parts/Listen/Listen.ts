e6513749671b416acb70f0e3e6c5b36e3bcb3161
Le Vivilet <levivilet@pm.me>
2025-12-08 22:09:05 +0100

    feature: add getCommandIds (#54)

--- packages/status-bar-worker/src/parts/Listen/Listen.ts
+++ packages/status-bar-worker/src/parts/Listen/Listen.ts
@@ -1,7 +1,9 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
+import { registerCommands } from '../StatusBarStates/StatusBarStates.ts'
 
 export const listen = async (): Promise<void> => {
+  registerCommands(CommandMap.commandMap)
   await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
