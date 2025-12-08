124d7cdf0df5a6708c0cb7f539077999ee2a50e6
Le Vivilet <levivilet@pm.me>
2025-12-08 16:27:03 +0100

    feature: add status bar states (#30)

---
+++ packages/status-bar-worker/src/parts/StatusBarStates/StatusBarStates.ts
@@ -0,0 +1,4 @@
+import * as ViewletRegistry from '@lvce-editor//viewlet-registry'
+import type { StatusBarState } from '../StatusBarState/StatusBarState.ts'
+
+export const { get, getCommandIds, registerCommands, set, wrapGetter } = ViewletRegistry.create<StatusBarState>()
