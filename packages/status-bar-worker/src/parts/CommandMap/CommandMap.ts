54c889c28e34911075bdd9237096095a85447794
Le Vivilet <levivilet@pm.me>
2025-12-08 16:32:22 +0100

    feature: wrap commands (#31)

--- packages/status-bar-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/status-bar-worker/src/parts/CommandMap/CommandMap.ts
@@ -5,13 +5,14 @@ import * as ItemLeftUpdate from '../ItemLeftUpdate/ItemLeftUpdate.ts'
 import * as ItemRightCreate from '../ItemRightCreate/ItemRightCreate.ts'
 import * as ItemRightUpdate from '../ItemRightUpdate/ItemRightUpdate.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
+import { wrapCommand } from '../StatusBarStates/StatusBarStates.ts'
 
 export const commandMap = {
   'StatusBar.create': StatusBar.create,
-  'StatusBar.handleClick': HandleClick.handleClick,
-  'StatusBar.itemLeftUpdate': ItemLeftUpdate.itemLeftUpdate,
-  'StatusBar.itemRightCreate': ItemRightCreate.itemRightCreate,
-  'StatusBar.itemRightUpdate': ItemRightUpdate.itemRightUpdate,
-  'StatusBar.loadContent': LoadContent.loadContent,
+  'StatusBar.handleClick': wrapCommand(HandleClick.handleClick),
+  'StatusBar.itemLeftUpdate': wrapCommand(ItemLeftUpdate.itemLeftUpdate),
+  'StatusBar.itemRightCreate': wrapCommand(ItemRightCreate.itemRightCreate),
+  'StatusBar.itemRightUpdate': wrapCommand(ItemRightUpdate.itemRightUpdate),
+  'StatusBar.loadContent': wrapCommand(LoadContent.loadContent),
   'StatusBar.terminate': terminate,
 }
