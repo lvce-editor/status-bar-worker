d5122416b599b6e785d3e291c9bc1903feff6ce5
Le Vivilet <levivilet@pm.me>
2025-12-08 17:08:10 +0100

    feature: add diff modules

---
+++ packages/status-bar-worker/src/parts/DiffType/DiffType.ts
@@ -0,0 +1,7 @@
+export const RenderItems = 4
+export const RenderEditingIndex = 5
+export const RenderFocus = 6
+export const RenderFocusContext = 7
+export const RenderValue = 8
+export const RenderSelection = 9
+export const RenderCss = 10
