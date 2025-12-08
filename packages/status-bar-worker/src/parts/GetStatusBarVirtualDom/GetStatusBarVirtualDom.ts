28a000f19dadc700f94bd9b76c7109d4cfa7da5d
Le Vivilet <levivilet@pm.me>
2025-12-08 16:14:31 +0100

    feature: add dom node types

--- packages/status-bar-worker/src/parts/GetStatusBarVirtualDom/GetStatusBarVirtualDom.ts
+++ packages/status-bar-worker/src/parts/GetStatusBarVirtualDom/GetStatusBarVirtualDom.ts
@@ -1,14 +1,18 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import type { StatusBarItem } from '../StatusBarItem/StatusBarItem.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetStatusBarItemsVirtualDom from '../GetStatusBarItemsVirtualDom/GetStatusBarItemsVirtualDom.ts'
 
-export const getStatusBarVirtualDom = (statusBarItemsLeft: readonly StatusBarItem[], statusBarItemsRight: readonly StatusBarItem[]): unknown[] => {
-  const dom: unknown[] = []
+export const getStatusBarVirtualDom = (
+  statusBarItemsLeft: readonly StatusBarItem[],
+  statusBarItemsRight: readonly StatusBarItem[],
+): readonly VirtualDomNode[] => {
+  const dom: VirtualDomNode[] = []
   if (statusBarItemsLeft.length > 0) {
     dom.push(...GetStatusBarItemsVirtualDom.getStatusBarItemsVirtualDom(statusBarItemsLeft, ClassNames.StatusBarItemsLeft))
   }
   if (statusBarItemsRight.length > 0) {
-    dom.push(GetStatusBarItemsVirtualDom.getStatusBarItemsVirtualDom(statusBarItemsRight, ClassNames.StatusBarItemsRight))
+    dom.push(...GetStatusBarItemsVirtualDom.getStatusBarItemsVirtualDom(statusBarItemsRight, ClassNames.StatusBarItemsRight))
   }
   return dom
 }
