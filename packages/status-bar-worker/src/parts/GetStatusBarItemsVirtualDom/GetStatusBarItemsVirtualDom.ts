3ea96e6dfb06018b46f1988be8c8e6c13eef5af6
Le Vivilet <levivilet@pm.me>
2025-12-08 16:24:39 +0100

    feature: use dom node types (#23)

--- packages/status-bar-worker/src/parts/GetStatusBarItemsVirtualDom/GetStatusBarItemsVirtualDom.ts
+++ packages/status-bar-worker/src/parts/GetStatusBarItemsVirtualDom/GetStatusBarItemsVirtualDom.ts
@@ -1,22 +1,7 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
-import { VirtualDomElements, AriaRoles, text } from '@lvce-editor/virtual-dom-worker'
+import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { StatusBarItem } from '../StatusBarItem/StatusBarItem.ts'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
-
-const getStatusBarItemVirtualDom = (statusBarItem: StatusBarItem): readonly VirtualDomNode[] => {
-  const { tooltip } = statusBarItem
-  return [
-    {
-      childCount: 1,
-      className: ClassNames.StatusBarItem,
-      role: AriaRoles.Button,
-      tabIndex: -1,
-      title: tooltip,
-      type: VirtualDomElements.Div,
-    },
-    text(statusBarItem.text),
-  ]
-}
+import * as GetStatusBarItemVirtualDom from '../GetStatusBarItemVirtualDom/GetStatusBarItemVirtualDom.ts'
 
 export const getStatusBarItemsVirtualDom = (items: readonly StatusBarItem[], className: string): readonly VirtualDomNode[] => {
   return [
@@ -25,6 +10,6 @@ export const getStatusBarItemsVirtualDom = (items: readonly StatusBarItem[], cla
       className,
       type: VirtualDomElements.Div,
     },
-    ...items.flatMap(getStatusBarItemVirtualDom),
+    ...items.flatMap(GetStatusBarItemVirtualDom.getStatusBarItemVirtualDom),
   ]
 }
