4c2027336d147b3cccbc5f5a86c25302ce025420
Le Vivilet <levivilet@pm.me>
2025-12-08 16:07:34 +0100

    lint

--- packages/status-bar-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/status-bar-worker/src/parts/LoadContent/LoadContent.ts
@@ -1,4 +1,3 @@
-import type { StatusBarItem } from '../StatusBarItem/StatusBarItem.ts'
 import type * as StatusBarState from '../StatusBarState/StatusBarState.ts'
 import * as GetStatusBarItems from '../GetStatusBarItems/GetStatusBarItems.ts'
 import * as StatusBarPreferences from '../StatusBarPreferences/StatusBarPreferences.ts'
@@ -14,22 +13,6 @@ export const loadContent = async (state: Readonly<State>): Promise<State> => {
     ...state,
     statusBarItemsLeft: [...statusBarItems],
   }
-}
-
-const updateArray = (items: readonly StatusBarItem[], newItem: Readonly<StatusBarItem>): StatusBarItem[] => {
-  const index = getIndex(items, newItem)
-  const before = items.slice(0, index)
-  const after = items.slice(index + 1)
-  return [...before, newItem, ...after]
-}
-
-const getIndex = (items: readonly StatusBarItem[], item: Readonly<StatusBarItem>): number => {
-  for (let i = 0; i < items.length; i++) {
-    if (items[i].name === item.name) {
-      return i
-    }
-  }
-  return -1
 }
 
 export { handleClick } from '../HandleClick/HandleClick.ts'
