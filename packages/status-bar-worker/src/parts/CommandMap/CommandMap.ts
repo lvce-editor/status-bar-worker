d50f7182281fe81b214bf1772a52a707594a8fa0
Le Vivilet <levivilet@pm.me>
2025-12-08 16:08:16 +0100

    register

--- packages/status-bar-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/status-bar-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,9 +1,19 @@
 import { terminate } from '@lvce-editor/viewlet-registry'
 import * as StatusBar from '../Create/Create.ts'
-import { loadContent } from '../LoadContent/LoadContent.ts'
+import {
+  handleClick,
+  itemLeftUpdate,
+  itemRightCreate,
+  itemRightUpdate,
+  loadContent,
+} from '../LoadContent/LoadContent.ts'
 
 export const commandMap = {
   'StatusBar.create': StatusBar.create,
   'StatusBar.loadContent': loadContent,
   'StatusBar.terminate': terminate,
+  'StatusBar.handleClick': handleClick,
+  'StatusBar.itemLeftUpdate': itemLeftUpdate,
+  'StatusBar.itemRightCreate': itemRightCreate,
+  'StatusBar.itemRightUpdate': itemRightUpdate,
 }
