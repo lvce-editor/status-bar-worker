84d11263787ca64df3f9fad156b19df141be9f02
Le Vivilet <levivilet@pm.me>
2025-12-08 15:48:08 +0100

    feature: register commands

--- packages/status-bar-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/status-bar-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,1 +1,9 @@
-export const commandMap = {}
+import { terminate } from '@lvce-editor/viewlet-registry'
+import * as StatusBar from '../Create/Create.ts'
+import { loadContent } from '../LoadContent/LoadContent.ts'
+
+export const commandMap = {
+  'StatusBar.create': StatusBar.create,
+  'StatusBar.loadContent': loadContent,
+  'StatusBar.terminate': terminate,
+}
