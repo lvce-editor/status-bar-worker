c3821c17222cbaffa9f169f05ab627ed5a60467b
Le Vivilet <levivilet@pm.me>
2025-12-09 13:34:56 +0100

    feature: add click listener

--- packages/status-bar-worker/src/parts/RenderEventListeners/RenderEventListeners.ts
+++ packages/status-bar-worker/src/parts/RenderEventListeners/RenderEventListeners.ts
@@ -5,47 +5,8 @@ import * as DomEventListenersFunctions from '../DomEventListenerFunctions/DomEve
 export const renderEventListeners = (): readonly DomEventListener[] => {
   return [
     {
-      name: DomEventListenersFunctions.HandleWheel,
-      params: ['handleWheel', EventExpression.DeltaMode, EventExpression.DeltaY],
-      passive: true,
-    },
-    {
-      name: DomEventListenersFunctions.HandleFocus,
-      params: ['handleFocus'],
-    },
-    {
-      name: DomEventListenersFunctions.HandleClickAt,
-      params: ['handleClickAt', EventExpression.ClientX, EventExpression.ClientY, EventExpression.TargetName],
-    },
-    {
-      name: DomEventListenersFunctions.HandleMouseOverAt,
-      params: ['handleMouseOverAt', EventExpression.ClientX, EventExpression.ClientY],
-    },
-    {
-      name: DomEventListenersFunctions.HandleMouseOver,
-      params: ['handleMouseOver', EventExpression.ClientX, EventExpression.ClientY],
-    },
-    {
-      name: DomEventListenersFunctions.HandleMouseOutAt,
-      params: ['handleMouseOutAt', EventExpression.ClientX, EventExpression.ClientY],
-    },
-    {
-      name: DomEventListenersFunctions.HandleInput,
-      params: ['handleInput', EventExpression.TargetValue],
-    },
-    {
-      name: DomEventListenersFunctions.HandleContextMenu,
-      params: ['handleContextMenu', EventExpression.Button, EventExpression.ClientX, EventExpression.ClientY],
-      preventDefault: true,
-    },
-    {
-      name: DomEventListenersFunctions.HandleWheel,
-      params: ['handleWheel', EventExpression.DeltaMode, EventExpression.DeltaY],
-      passive: true,
-    },
-    {
-      name: DomEventListenersFunctions.HandleClickAction,
-      params: ['handleActionClick', EventExpression.TargetName],
+      name: DomEventListenersFunctions.HandleClick,
+      params: ['handleClick', EventExpression.TargetName],
     },
   ]
 }
