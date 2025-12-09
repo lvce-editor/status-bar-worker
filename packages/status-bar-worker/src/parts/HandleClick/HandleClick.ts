65cfc7f2a14d1610012784388f259fddc6e31fda
Le Vivilet <levivilet@pm.me>
2025-12-09 13:40:25 +0100

    return

--- packages/status-bar-worker/src/parts/HandleClick/HandleClick.ts
+++ packages/status-bar-worker/src/parts/HandleClick/HandleClick.ts
@@ -1,6 +1,30 @@
+import type { StatusBarItem } from '../StatusBarItem/StatusBarItem.ts'
 import type { StatusBarState } from '../StatusBarState/StatusBarState.ts'
 
+const getMatchingItem = (itemsLeft: readonly StatusBarItem[], itemsRight: readonly StatusBarItem[], name: string): StatusBarItem | undefined => {
+  for (const item of itemsLeft) {
+    if (item.name === name) {
+      return item
+    }
+  }
+  for (const item of itemsRight) {
+    if (item.name === name) {
+      return item
+    }
+  }
+  return undefined
+}
+
 export const handleClick = async (state: StatusBarState, name: string): Promise<StatusBarState> => {
+  if (!name) {
+    return state
+  }
+  const { statusBarItemsLeft, statusBarItemsRight } = state
+
+  const item = getMatchingItem(statusBarItemsLeft, statusBarItemsRight, name)
+  if (!item) {
+    return state
+  }
   // TODO
   // sendExtensionWorker([/* statusBarItemHandleClick */ 7657, /* name */ name])
   return state
