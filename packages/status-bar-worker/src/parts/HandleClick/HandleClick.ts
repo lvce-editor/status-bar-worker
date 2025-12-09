ef141077880108695b588aba0ff25a6115be8402
Le Vivilet <levivilet@pm.me>
2025-12-09 13:46:44 +0100

    feature: improve click handler (#63)

--- packages/status-bar-worker/src/parts/HandleClick/HandleClick.ts
+++ packages/status-bar-worker/src/parts/HandleClick/HandleClick.ts
@@ -1,5 +1,6 @@
 import type { StatusBarItem } from '../StatusBarItem/StatusBarItem.ts'
 import type { StatusBarState } from '../StatusBarState/StatusBarState.ts'
+import * as InputName from '../InputName/InputName.ts'
 
 const getMatchingItem = (itemsLeft: readonly StatusBarItem[], itemsRight: readonly StatusBarItem[], name: string): StatusBarItem | undefined => {
   for (const item of itemsLeft) {
@@ -15,6 +16,10 @@ const getMatchingItem = (itemsLeft: readonly StatusBarItem[], itemsRight: readon
   return undefined
 }
 
+const handleClickNotification = async (): Promise<void> => {
+  // TODO toggle notifications
+}
+
 export const handleClick = async (state: StatusBarState, name: string): Promise<StatusBarState> => {
   if (!name) {
     return state
@@ -24,6 +29,9 @@ export const handleClick = async (state: StatusBarState, name: string): Promise<
   const item = getMatchingItem(statusBarItemsLeft, statusBarItemsRight, name)
   if (!item) {
     return state
+  }
+  if (item.name === InputName.Notifications) {
+    await handleClickNotification()
   }
   // TODO
   // sendExtensionWorker([/* statusBarItemHandleClick */ 7657, /* name */ name])
