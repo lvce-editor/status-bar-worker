fa1ceb4ad57a97479601ca4f34ca3f503eba2936
Le Vivilet <levivilet@pm.me>
2025-12-09 13:41:51 +0100

    feature: update click function (#61)

--- packages/status-bar-worker/src/parts/HandleClick/HandleClick.ts
+++ packages/status-bar-worker/src/parts/HandleClick/HandleClick.ts
@@ -1,10 +1,30 @@
-import type * as StatusBarState from '../StatusBarState/StatusBarState.ts'
+import type { StatusBarItem } from '../StatusBarItem/StatusBarItem.ts'
+import type { StatusBarState } from '../StatusBarState/StatusBarState.ts'
 
-type State = StatusBarState.StatusBarState & {
-  disposed?: boolean
+const getMatchingItem = (itemsLeft: readonly StatusBarItem[], itemsRight: readonly StatusBarItem[], name: string): StatusBarItem | undefined => {
+  for (const item of itemsLeft) {
+    if (item.name === name) {
+      return item
+    }
+  }
+  for (const item of itemsRight) {
+    if (item.name === name) {
+      return item
+    }
+  }
+  return undefined
 }
 
-export const handleClick = (state: Readonly<State>, name: string): State => {
+export const handleClick = async (state: StatusBarState, name: string): Promise<StatusBarState> => {
+  if (!name) {
+    return state
+  }
+  const { statusBarItemsLeft, statusBarItemsRight } = state
+
+  const item = getMatchingItem(statusBarItemsLeft, statusBarItemsRight, name)
+  if (!item) {
+    return state
+  }
   // TODO
   // sendExtensionWorker([/* statusBarItemHandleClick */ 7657, /* name */ name])
   return state
