57b6238d9955970ab11f4b4faca5d25da6e636f7
Le Vivilet <levivilet@pm.me>
2025-12-09 15:10:57 +0100

    feature: use button element for status bar items

--- packages/status-bar-worker/src/parts/GetStatusBarItemVirtualDom/GetStatusBarItemVirtualDom.ts
+++ packages/status-bar-worker/src/parts/GetStatusBarItemVirtualDom/GetStatusBarItemVirtualDom.ts
@@ -13,7 +13,7 @@ export const getStatusBarItemVirtualDom = (statusBarItem: StatusBarItem): readon
       role: AriaRoles.Button,
       tabIndex: -1,
       title: tooltip,
-      type: VirtualDomElements.Div,
+      type: VirtualDomElements.Button,
     },
     text(statusBarItem.text),
   ]
