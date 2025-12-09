e0d8b36a6b81d5804a7f8d0a096b619de18d6183
Le Vivilet <levivilet@pm.me>
2025-12-09 12:58:34 +0100

    add item

--- packages/status-bar-worker/src/parts/StatusBarPreferences/StatusBarPreferences.ts
+++ packages/status-bar-worker/src/parts/StatusBarPreferences/StatusBarPreferences.ts
@@ -1,6 +1,6 @@
 import * as Preferences from '../Preferences/Preferences.ts'
 
 export const itemsVisible = async (): Promise<boolean> => {
-  const statusBarItemsPreference = (await Preferences.get('statusBar.itemsVisible')) ?? false
+  const statusBarItemsPreference = (await Preferences.get('statusBar.itemsVisible')) ?? true
   return statusBarItemsPreference
 }
