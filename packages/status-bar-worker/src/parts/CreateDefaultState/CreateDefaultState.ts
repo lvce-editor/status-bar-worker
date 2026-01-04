b2017e36a282e6cc4de8bd66e9b9b1e0ecb2a8b8
Le Vivilet <levivilet@pm.me>
2026-01-04 14:37:59 +0100

    fix

--- packages/status-bar-worker/src/parts/CreateDefaultState/CreateDefaultState.ts
+++ packages/status-bar-worker/src/parts/CreateDefaultState/CreateDefaultState.ts
@@ -2,8 +2,8 @@ import type { StatusBarState } from '../StatusBarState/StatusBarState.ts'
 
 export const createDefaultState = (): StatusBarState => {
   return {
-    assetDir,
-    platform,
+    assetDir: '',
+    platform: 0,
     statusBarItemsLeft: [],
     statusBarItemsRight: [],
     uid: 0,
