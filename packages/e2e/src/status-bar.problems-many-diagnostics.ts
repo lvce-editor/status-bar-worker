d716059d3ab48df7f0e4c6c4072c2b5e117d7d13
Le Vivilet <levivilet@pm.me>
2026-01-20 17:45:50 +0100

    feature: add more e2e tests

---
+++ packages/e2e/src/status-bar.problems-many-diagnostics.ts
@@ -0,0 +1,17 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'status-bar.problems-many-diagnostics'
+
+export const test: Test = async ({ expect, Locator }) => {
+  const statusBar = Locator('.StatusBar')
+  await expect(statusBar).toBeVisible()
+
+  const itemProblems = Locator('.StatusBarItem[name="Problems"]')
+  await expect(itemProblems).toBeVisible()
+
+  const errorIcon = itemProblems.locator('.ProblemsErrorIcon')
+  await expect(errorIcon).toBeVisible()
+
+  const warningIcon = itemProblems.locator('.ProblemsWarningIcon')
+  await expect(warningIcon).toBeVisible()
+}
