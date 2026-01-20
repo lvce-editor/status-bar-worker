c265b4e87ec91ab47177338f325c36a219b97911
Le Vivilet <levivilet@pm.me>
2026-01-20 17:07:17 +0100

    feature: move getBuiltinStatusBarItems into separate file

--- packages/status-bar-worker/src/parts/GetStatusBarItems/GetStatusBarItems.ts
+++ packages/status-bar-worker/src/parts/GetStatusBarItems/GetStatusBarItems.ts
@@ -1,36 +1,17 @@
 import type { StatusBarItem } from '../StatusBarItem/StatusBarItem.ts'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as ExtensionHostStatusBarItems from '../ExtensionHost/ExtensionHostStatusBarItems.ts'
 import * as ExtensionHostManagement from '../ExtensionHostManagement/ExtensionHostManagement.ts'
-import * as InputName from '../InputName/InputName.ts'
+import { getBuiltinStatusBarItems } from '../GetBuiltinStatusBarItems/GetBuiltinStatusBarItems.ts'
 import * as ToStatusBarItem from '../ToStatusBarItem/ToStatusBarItem.ts'
 import * as ToUiStatusBarItems from '../ToUiStatusBarItems/ToUiStatusBarItems.ts'
 
-export const getStatusBarItems = async (showItems: boolean, assetDir: string, platform: number): Promise<StatusBarItem[]> => {
+export const getStatusBarItems = async (showItems: boolean, assetDir: string, platform: number): Promise<readonly StatusBarItem[]> => {
   if (!showItems) {
     return []
   }
   await ExtensionHostManagement.activateByEvent('onSourceControl', assetDir, platform)
   const extensionStatusBarItems = await ExtensionHostStatusBarItems.getStatusBarItems(assetDir, platform)
   const uiStatusBarItems = ToUiStatusBarItems.toUiStatusBarItems(extensionStatusBarItems)
-  const extraItems: readonly StatusBarItem[] = [
-    {
-      command: undefined,
-      elements: [{ type: 'text', value: 'Notifications' }],
-      name: InputName.Notifications,
-      tooltip: '',
-    },
-    {
-      command: undefined,
-      elements: [
-        { type: 'icon', value: ClassNames.ProblemsErrorIcon },
-        { type: 'text', value: '0' },
-        { type: 'icon', value: ClassNames.ProblemsWarningIcon },
-        { type: 'text', value: '0' },
-      ],
-      name: InputName.Problems,
-      tooltip: '',
-    },
-  ]
+  const extraItems = await getBuiltinStatusBarItems()
   return [...uiStatusBarItems.map(ToStatusBarItem.toStatusBarItem), ...extraItems]
 }
