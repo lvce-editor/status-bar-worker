83892720d5216b0a6fcfec004c41218af54fe066
Le Vivilet <levivilet@pm.me>
2026-01-20 17:13:43 +0100

    feature: move element dom into separate file (#88)

--- packages/status-bar-worker/src/parts/GetStatusBarItemVirtualDom/GetStatusBarItemVirtualDom.ts
+++ packages/status-bar-worker/src/parts/GetStatusBarItemVirtualDom/GetStatusBarItemVirtualDom.ts
@@ -1,28 +1,12 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
-import { VirtualDomElements, AriaRoles, text } from '@lvce-editor/virtual-dom-worker'
+import { AriaRoles, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { StatusBarItem } from '../StatusBarItem/StatusBarItem.ts'
-import type { StatusBarItemElement } from '../StatusBarItemElement/StatusBarItemElement.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-
-const getElementVirtualDom = (element: StatusBarItemElement): readonly VirtualDomNode[] => {
-  if (element.type === 'text') {
-    return [text(element.value)]
-  }
-  if (element.type === 'icon') {
-    return [
-      {
-        childCount: 0,
-        className: element.value,
-        type: VirtualDomElements.Div,
-      },
-    ]
-  }
-  return []
-}
+import { getStatusBarItemElementVirtualDom } from '../GetStatusBarItemElementVirtualDom/GetStatusBarItemElementVirtualDom.ts'
 
 export const getStatusBarItemVirtualDom = (statusBarItem: StatusBarItem): readonly VirtualDomNode[] => {
   const { elements, name, tooltip } = statusBarItem
-  const elementNodes = elements.flatMap(getElementVirtualDom)
+  const elementNodes = elements.flatMap(getStatusBarItemElementVirtualDom)
   return [
     {
       childCount: elementNodes.length,
