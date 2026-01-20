8060b98ec5a0adf40e13dd7b2d56ae7ab02e429d
Le Vivilet <levivilet@pm.me>
2026-01-20 17:49:49 +0100

    feature: update interface

--- packages/status-bar-worker/src/parts/StatusBarItemElement/StatusBarItemElement.ts
+++ packages/status-bar-worker/src/parts/StatusBarItemElement/StatusBarItemElement.ts
@@ -1,9 +1,15 @@
-export type StatusBarItemElement =
-  | {
-      readonly type: 'text'
-      readonly value: string
-    }
-  | {
-      readonly type: 'icon'
-      readonly value: string
-    }
+interface StatusBarItemBase {
+  readonly type: string
+}
+
+export interface StatusBarItemText extends StatusBarItemBase {
+  readonly type: 'text'
+  readonly value: string
+}
+
+export interface StatusBarItemIcon extends StatusBarItemBase {
+  readonly type: 'icon'
+  readonly value: string
+}
+
+export type StatusBarItemElement = StatusBarItemText | StatusBarItemIcon
