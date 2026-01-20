317838531bbad437ed6de9968df5c4d1bb6c8b1e
Le Vivilet <levivilet@pm.me>
2026-01-20 17:52:14 +0100

    feature: update classname for builtin status bar items

--- packages/status-bar-worker/src/parts/GetBuiltinStatusBarItems/GetBuiltinStatusBarItems.ts
+++ packages/status-bar-worker/src/parts/GetBuiltinStatusBarItems/GetBuiltinStatusBarItems.ts
@@ -1,25 +1,26 @@
 import type { StatusBarItem } from '../StatusBarItem/StatusBarItem.ts'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as InputName from '../InputName/InputName.ts'
 
 export const getBuiltinStatusBarItems = async (): Promise<readonly StatusBarItem[]> => {
+  const errorCount = 0
+  const warningCount = 0
   const extraItems: readonly StatusBarItem[] = [
     {
-      command: undefined,
+      command: '', // TODO should show notifications center
       elements: [{ type: 'text', value: 'Notifications' }],
       name: InputName.Notifications,
-      tooltip: '',
+      tooltip: 'Notifications',
     },
     {
-      command: undefined,
+      command: '', // TODO should show problems view
       elements: [
-        { type: 'icon', value: ClassNames.ProblemsErrorIcon },
-        { type: 'text', value: '0' },
-        { type: 'icon', value: ClassNames.ProblemsWarningIcon },
-        { type: 'text', value: '0' },
+        { type: 'icon', value: `MaskIcon MaskIconError` },
+        { type: 'text', value: `${errorCount}` },
+        { type: 'icon', value: `MaskIcon MaskIconWarning` },
+        { type: 'text', value: `${warningCount}` },
       ],
       name: InputName.Problems,
-      tooltip: '',
+      tooltip: 'Problems',
     },
   ]
   return extraItems
