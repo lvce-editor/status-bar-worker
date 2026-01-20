a01dbd2b8fba9e381ada7852125000c2cda32b9c
Le Vivilet <levivilet@pm.me>
2026-01-20 17:09:39 +0100

    feature: add errorCount property (#86)

--- packages/status-bar-worker/src/parts/Create/Create.ts
+++ packages/status-bar-worker/src/parts/Create/Create.ts
@@ -4,10 +4,12 @@ import { set } from '../StatusBarStates/StatusBarStates.ts'
 export const create = (uid: number, uri: string, x: number, y: number, width: number, height: number, platform: number, assetDir: string): void => {
   const state: StatusBarState = {
     assetDir,
+    errorCount: 0,
     platform,
     statusBarItemsLeft: [],
     statusBarItemsRight: [],
     uid,
+    warningCount: 0,
   }
   set(uid, state, state)
 }
