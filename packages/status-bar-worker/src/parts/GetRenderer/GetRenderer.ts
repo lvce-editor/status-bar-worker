591c4b59e7dd95ebde09939668a908ba30e8110c
Le Vivilet <levivilet@pm.me>
2026-01-29 18:13:07 +0100

    feature: implement incremental rendering logic (#108)

--- packages/status-bar-worker/src/parts/GetRenderer/GetRenderer.ts
+++ packages/status-bar-worker/src/parts/GetRenderer/GetRenderer.ts
@@ -1,9 +1,12 @@
 import type { Renderer } from '../Renderer/Renderer.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
+import { renderIcremental } from '../RenderIncremental/RenderIncremental.ts'
 import * as RenderItems from '../RenderItems/RenderItems.ts'
 
 export const getRenderer = (diffType: number): Renderer => {
   switch (diffType) {
+    case DiffType.RenderIncremental:
+      return renderIcremental
     case DiffType.RenderItems:
       return RenderItems.renderItems
     default:
