0ab1217e1cfdeae47ddc289f636b3945ea6cce81
Le Vivilet <levivilet@pm.me>
2026-01-29 18:51:13 +0100

    feature: enable incremental renderer

--- packages/status-bar-worker/src/parts/RenderItems/RenderItems.ts
+++ packages/status-bar-worker/src/parts/RenderItems/RenderItems.ts
@@ -3,7 +3,10 @@ import type { StatusBarState } from '../StatusBarState/StatusBarState.ts'
 import { getStatusBarVirtualDom } from '../GetStatusBarVirtualDom/GetStatusBarVirtualDom.ts'
 
 export const renderItems = (oldState: StatusBarState, newState: StatusBarState): any => {
-  const { statusBarItemsLeft, statusBarItemsRight, uid } = newState
+  const { initial, statusBarItemsLeft, statusBarItemsRight, uid } = newState
+  if (initial) {
+    return [ViewletCommand.SetDom2, uid, []]
+  }
   const dom = getStatusBarVirtualDom(statusBarItemsLeft, statusBarItemsRight)
   return [ViewletCommand.SetDom2, uid, dom]
 }
