5043e981094164edfc7a40cbc26d8d2150ec91d3
Le Vivilet <levivilet@pm.me>
2026-01-29 16:50:32 +0100

    feature: add problems test (#102)

--- packages/status-bar-worker/test/HandleClickProblems.test.ts
+++ packages/status-bar-worker/test/HandleClickProblems.test.ts
@@ -2,35 +2,35 @@ import { expect, test } from '@jest/globals'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as HandleClickProblems from '../src/parts/HandleClickProblems/HandleClickProblems.ts'
 
-test.skip('handleClickProblems should call Layout.showPanel', async () => {
+test('handleClickProblems should call Layout.showPanel', async () => {
   using mockRpc = RendererWorker.registerMockRpc({
     'Layout.showPanel': async () => {},
-    'Panel.selectIndex': async () => {},
+    'Panel.toggleView': async () => {},
   })
 
   await HandleClickProblems.handleClickProblems()
 
-  expect(mockRpc.invocations).toEqual([['Layout.showPanel'], ['Panel.selectIndex', 1]])
+  expect(mockRpc.invocations).toContainEqual(['Layout.showPanel'])
 })
 
-test.skip('handleClickProblems should call Panel.selectIndex with 1', async () => {
+test('handleClickProblems should call Panel.toggleView with Problems', async () => {
   using mockRpc = RendererWorker.registerMockRpc({
     'Layout.showPanel': async () => {},
-    'Panel.selectIndex': async () => {},
+    'Panel.toggleView': async () => {},
   })
 
   await HandleClickProblems.handleClickProblems()
 
-  expect(mockRpc.invocations).toEqual([['Layout.showPanel'], ['Panel.selectIndex', 1]])
+  expect(mockRpc.invocations).toContainEqual(['Panel.toggleView', 'Problems'])
 })
 
-test.skip('handleClickProblems should call Layout.showPanel and Panel.selectIndex in order', async () => {
+test('handleClickProblems should call Layout.showPanel and Panel.toggleView in order', async () => {
   using mockRpc = RendererWorker.registerMockRpc({
     'Layout.showPanel': async () => {},
-    'Panel.selectIndex': async () => {},
+    'Panel.toggleView': async () => {},
   })
 
   await HandleClickProblems.handleClickProblems()
 
-  expect(mockRpc.invocations).toEqual([['Layout.showPanel'], ['Panel.selectIndex', 1]])
+  expect(mockRpc.invocations).toEqual([['Layout.showPanel'], ['Panel.toggleView', 'Problems']])
 })
