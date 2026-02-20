1376e8fc070a27095324d5b6611dac737d663e4a
Le Vivilet <levivilet@pm.me>
2026-02-20 23:01:56 +0100

    typo

--- packages/status-bar-worker/src/parts/RenderIncremental/RenderIncremental.ts
+++ packages/status-bar-worker/src/parts/RenderIncremental/RenderIncremental.ts
@@ -3,7 +3,7 @@ import { diffTree } from '@lvce-editor/virtual-dom-worker'
 import type { StatusBarState } from '../StatusBarState/StatusBarState.ts'
 import { renderItems } from '../RenderItems/RenderItems.ts'
 
-export const renderIcremental = (oldState: StatusBarState, newState: StatusBarState): any => {
+export const renderIncremental = (oldState: StatusBarState, newState: StatusBarState): any => {
   const oldDom = renderItems(oldState, oldState)[2]
   const newDom = renderItems(newState, newState)[2]
   const patches = diffTree(oldDom, newDom)
