cb1ba722513f4fbf557d5f5c81bb940ad64723aa
Le Vivilet <levivilet@pm.me>
2026-05-27 01:16:15 +0200

    refactor: simplify getMatchingItem logic by extracting internal function (#132)

--- packages/status-bar-worker/src/parts/GetMatchingItem/GetMatchingItem.ts
+++ packages/status-bar-worker/src/parts/GetMatchingItem/GetMatchingItem.ts
@@ -1,19 +1,18 @@
 import type { StatusBarItem } from '../StatusBarItem/StatusBarItem.ts'
 
-export const getMatchingItem = (
-  itemsLeft: readonly StatusBarItem[],
-  itemsRight: readonly StatusBarItem[],
-  name: string,
-): StatusBarItem | undefined => {
-  for (const item of itemsLeft) {
-    if (item.name === name) {
-      return item
-    }
-  }
-  for (const item of itemsRight) {
+const getMatchingItemInternal = (items: readonly StatusBarItem[], name: string): StatusBarItem | undefined => {
+  for (const item of items) {
     if (item.name === name) {
       return item
     }
   }
   return undefined
+}
+
+export const getMatchingItem = (
+  itemsLeft: readonly StatusBarItem[],
+  itemsRight: readonly StatusBarItem[],
+  name: string,
+): StatusBarItem | undefined => {
+  return getMatchingItemInternal(itemsLeft, name) ?? getMatchingItemInternal(itemsRight, name)
 }
