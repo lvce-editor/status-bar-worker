cb56a821b5073dc8a28713e552f2abde4ff9289a
Le Vivilet <levivilet@pm.me>
2026-05-27 01:34:26 +0200

    feature: implement initializeRenderWorker function and integrate into listen (#137)

--- packages/status-bar-worker/src/parts/Listen/Listen.ts
+++ packages/status-bar-worker/src/parts/Listen/Listen.ts
@@ -1,12 +1,8 @@
-import { WebWorkerRpcClient } from '@lvce-editor/rpc'
-import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
+import { initializeRenderWorker } from '../InitializeRenderWorker/InitializeRenderWorker.ts'
 import { registerCommands } from '../StatusBarStates/StatusBarStates.ts'
 
 export const listen = async (): Promise<void> => {
   registerCommands(CommandMap.commandMap)
-  const rpc = await WebWorkerRpcClient.create({
-    commandMap: CommandMap.commandMap,
-  })
-  RendererWorker.set(rpc)
+  await initializeRenderWorker()
 }
