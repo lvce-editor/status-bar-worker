25fd125d5bd8db0aaa66d37c174160d2b5efad1a
Le Vivilet <levivilet@pm.me>
2026-05-26 20:24:22 +0200

    feature: implement handleExtensionsChanged to requery status bar items on extension changes (#127)

--- packages/status-bar-worker/src/parts/HandleExtensionsChanged/HandleExtensionsChanged.ts
+++ packages/status-bar-worker/src/parts/HandleExtensionsChanged/HandleExtensionsChanged.ts
@@ -1,6 +1,20 @@
 import type { StatusBarState } from '../StatusBarState/StatusBarState.ts'
+import * as GetStatusBarItems from '../GetStatusBarItems/GetStatusBarItems.ts'
 
 export const handleExtensionsChanged = async (state: StatusBarState): Promise<StatusBarState> => {
+  const { assetDir, errorCount, platform, warningCount } = state
   // TODO requery status bar items
-  return state
+  const statusBarItems = await GetStatusBarItems.getStatusBarItems({
+    assetDir,
+    builtinNotificationsEnabled: false,
+    builtinProblemsEnabled: false,
+    errorCount,
+    platform,
+    showItems: true,
+    warningCount,
+  })
+  return {
+    ...state,
+    statusBarItemsLeft: [...statusBarItems],
+  }
 }
