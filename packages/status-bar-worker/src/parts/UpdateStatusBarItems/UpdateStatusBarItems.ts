07b299accf54853214e6f240355751da0dbc4c28
Le Vivilet <levivilet@pm.me>
2026-05-27 01:38:19 +0200

    refactor: update type import and simplify state definition in updateStatusBarItems function (#140)

--- packages/status-bar-worker/src/parts/UpdateStatusBarItems/UpdateStatusBarItems.ts
+++ packages/status-bar-worker/src/parts/UpdateStatusBarItems/UpdateStatusBarItems.ts
@@ -1,11 +1,7 @@
-import type * as StatusBarState from '../StatusBarState/StatusBarState.ts'
+import type { StatusBarState } from '../StatusBarState/StatusBarState.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
 
-type State = StatusBarState.StatusBarState & {
-  disposed?: boolean
-}
-
-export const updateStatusBarItems = async (state: Readonly<State>): Promise<State> => {
+export const updateStatusBarItems = async (state: StatusBarState): Promise<StatusBarState> => {
   const newState = await LoadContent.loadContent(state)
   return newState
 }
