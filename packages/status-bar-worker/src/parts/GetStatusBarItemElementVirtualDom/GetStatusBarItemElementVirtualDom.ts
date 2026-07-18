8b2b44bb4d341f51fc3cd9f3eb802995fb332a33
Le Vivilet <levivilet@pm.me>
2026-07-18 00:37:42 +0000

    fix: handle clicks on status bar item content

--- packages/status-bar-worker/src/parts/GetStatusBarItemElementVirtualDom/GetStatusBarItemElementVirtualDom.ts
+++ packages/status-bar-worker/src/parts/GetStatusBarItemElementVirtualDom/GetStatusBarItemElementVirtualDom.ts
@@ -2,33 +2,35 @@ import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { ClassNames, mergeClassNames, text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { StatusBarItemElement } from '../StatusBarItemElement/StatusBarItemElement.ts'
 
-const getTextVirtualDom = (element: StatusBarItemElement): readonly VirtualDomNode[] => {
+const getTextVirtualDom = (element: StatusBarItemElement, name: string): readonly VirtualDomNode[] => {
   return [
     {
       childCount: 1,
       className: 'StatusBarItemLabel',
+      name,
       type: VirtualDomElements.Span,
     },
     text(element.value),
   ]
 }
 
-const getIconVirtualDom = (element: StatusBarItemElement): readonly VirtualDomNode[] => {
+const getIconVirtualDom = (element: StatusBarItemElement, name: string): readonly VirtualDomNode[] => {
   return [
     {
       childCount: 0,
       className: mergeClassNames(ClassNames.MaskIcon, element.value),
+      name,
       type: VirtualDomElements.Div,
     },
   ]
 }
 
-export const getStatusBarItemElementVirtualDom = (element: StatusBarItemElement): readonly VirtualDomNode[] => {
+export const getStatusBarItemElementVirtualDom = (element: StatusBarItemElement, name: string): readonly VirtualDomNode[] => {
   if (element.type === 'text') {
-    return getTextVirtualDom(element)
+    return getTextVirtualDom(element, name)
   }
   if (element.type === 'icon') {
-    return getIconVirtualDom(element)
+    return getIconVirtualDom(element, name)
   }
   return []
 }
