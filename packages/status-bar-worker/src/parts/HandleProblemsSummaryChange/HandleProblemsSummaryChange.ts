52030a69339f56b9f890920bef28ecb81977d5c2
Le Vivilet <levivilet@pm.me>
2026-08-11 18:08:12 +0000

    fix: satisfy lint rules

--- packages/status-bar-worker/src/parts/HandleProblemsSummaryChange/HandleProblemsSummaryChange.ts
+++ packages/status-bar-worker/src/parts/HandleProblemsSummaryChange/HandleProblemsSummaryChange.ts
@@ -9,13 +9,14 @@ interface ProblemsSummary {
 }
 
 export const handleProblemsSummaryChange = (state: StatusBarState, summary: ProblemsSummary): StatusBarState => {
+  const { errorCount: oldErrorCount, statusBarItemsLeft: oldStatusBarItemsLeft, warningCount: oldWarningCount } = state
   const errorCount = summary.hasEditor ? summary.errorCount : 0
   const warningCount = summary.hasEditor ? summary.warningCount : 0
-  if (state.errorCount === errorCount && state.warningCount === warningCount) {
+  if (oldErrorCount === errorCount && oldWarningCount === warningCount) {
     return state
   }
   const problemsItem = getProblemsStatusBarItem(errorCount, warningCount, true)[0]
-  const statusBarItemsLeft = state.statusBarItemsLeft.map((item) => (item.name === InputName.Problems ? problemsItem : item))
+  const statusBarItemsLeft = oldStatusBarItemsLeft.map((item) => (item.name === InputName.Problems ? problemsItem : item))
   return {
     ...state,
     errorCount,
