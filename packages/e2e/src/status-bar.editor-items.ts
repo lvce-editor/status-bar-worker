1753640e139517984b450a1418727cd3d7e4e2de
Le Vivilet <levivilet@pm.me>
2026-08-18 16:15:04 +0000

    feature: enable editor status bar e2e test

--- packages/e2e/src/status-bar.editor-items.ts
+++ packages/e2e/src/status-bar.editor-items.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'status-bar.editor-items'
 
-export const skip = 1
-
 export const test: Test = async ({ Editor, expect, FileSystem, Locator, Main, Workspace }) => {
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(`${tmpDir}/status-bar.ts`, 'first\nsecond line')
@@ -16,7 +14,7 @@ export const test: Test = async ({ Editor, expect, FileSystem, Locator, Main, Wo
   const language = Locator('.StatusBarItem[name="EditorLanguage"]')
   const utf8Display = ['UTF', '8'].join('-')
   await expect(position).toHaveText('Ln 1, Col 1')
-  await expect(indentation).toHaveText('Spaces: 4')
+  await expect(indentation).toHaveText('Spaces: 2')
   await expect(encoding).toHaveText(utf8Display)
   await expect(language).toHaveText('typescript')
 
