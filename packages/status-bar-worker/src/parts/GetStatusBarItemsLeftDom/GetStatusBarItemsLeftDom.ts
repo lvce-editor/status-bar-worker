ad9fe2dfded33b583833fb4df8bd0367607570c5
Le Vivilet <levivilet@pm.me>
2026-08-20 13:11:12 +0000

    fix: hoist empty status bar dom

--- packages/status-bar-worker/src/parts/GetStatusBarItemsLeftDom/GetStatusBarItemsLeftDom.ts
+++ packages/status-bar-worker/src/parts/GetStatusBarItemsLeftDom/GetStatusBarItemsLeftDom.ts
@@ -3,15 +3,17 @@ import type { StatusBarItem } from '../StatusBarItem/StatusBarItem.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetStatusBarItemsVirtualDom from '../GetStatusBarItemsVirtualDom/GetStatusBarItemsVirtualDom.ts'
 
+const emptyStatusBarItemsLeftDom: readonly VirtualDomNode[] = [
+  {
+    childCount: 0,
+    className: ClassNames.StatusBarItemsLeft,
+    type: VirtualDomElements.Div,
+  },
+]
+
 export const getStatusBarItemsLeftDom = (statusBarItemsLeft: readonly StatusBarItem[]): readonly VirtualDomNode[] => {
   if (statusBarItemsLeft.length === 0) {
-    return [
-      {
-        childCount: 0,
-        className: ClassNames.StatusBarItemsLeft,
-        type: VirtualDomElements.Div,
-      },
-    ]
+    return emptyStatusBarItemsLeftDom
   }
   return GetStatusBarItemsVirtualDom.getStatusBarItemsVirtualDom(statusBarItemsLeft, ClassNames.StatusBarItemsLeft)
 }
