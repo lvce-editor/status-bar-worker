9fe5af586e3a24237f53fab781378b79683840ef
Le Vivilet <levivilet@pm.me>
2026-08-22 07:54:55 -0700

    Contribute status bar settings (#190)

--- packages/build/src/build.js
+++ packages/build/src/build.js
@@ -70,5 +70,6 @@ packageJson.main = 'dist/statusBarWorkerMain.js'
 
 await writeJson(join(dist, 'package.json'), packageJson)
 
+await cp(join(root, 'packages', 'status-bar-worker', 'settings.json'), join(dist, 'dist', 'settings.json'))
 await cp(join(root, 'README.md'), join(dist, 'README.md'))
 await cp(join(root, 'LICENSE'), join(dist, 'LICENSE'))
