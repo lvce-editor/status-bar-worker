4a60f3520a3ebe4a113bf94d95387b43cd4ee217
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-12 13:16:15 +0000

    fix: use current context menu selectors

--- packages/e2e/src/status-bar.context-menu-dismiss-escape.ts
+++ packages/e2e/src/status-bar.context-menu-dismiss-escape.ts
@@ -5,7 +5,7 @@ export const name = 'status-bar.context-menu-dismiss-escape'
 export const test: Test = async ({ expect, KeyBoard, Locator, StatusBar }) => {
   await StatusBar.handleContextMenu(0, 0, 0)
 
-  const hideStatusBar = Locator('.ContextMenuItem[title="Hide Status Bar"]')
+  const hideStatusBar = Locator('.MenuItem', { hasText: 'Hide Status Bar' })
   await expect(hideStatusBar).toBeVisible()
   await KeyBoard.press('Escape')
   await expect(hideStatusBar).toHaveCount(0)
--- packages/e2e/src/status-bar.context-menu.ts
+++ packages/e2e/src/status-bar.context-menu.ts
@@ -5,6 +5,6 @@ export const name = 'status-bar.context-menu'
 export const test: Test = async ({ expect, Locator, StatusBar }) => {
   await StatusBar.handleContextMenu(0, 0, 0)
 
-  const hideStatusBar = Locator('.ContextMenuItem[title="Hide Status Bar"]')
+  const hideStatusBar = Locator('.MenuItem', { hasText: 'Hide Status Bar' })
   await expect(hideStatusBar).toBeVisible()
 }
