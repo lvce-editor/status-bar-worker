e12c2b57880dbd82feea908ae950f1320e46864b
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-23 05:05:25 +0000

    style: format status bar context menu handler

--- packages/status-bar-worker/src/parts/HandleContextMenu/HandleContextMenu.ts
+++ packages/status-bar-worker/src/parts/HandleContextMenu/HandleContextMenu.ts
@@ -5,9 +5,10 @@ import * as MenuEntryId from '../MenuEntryId/MenuEntryId.ts'
 
 export const handleContextMenu = async (state: StatusBarState, button: number, x: number, y: number, targetName = ''): Promise<StatusBarState> => {
   const item = [...state.statusBarItemsLeft, ...state.statusBarItemsRight].find((statusBarItem) => statusBarItem.name === targetName)
-  const contextMenuItems = item?.extensionId && item.providerId
-    ? await GetStatusBarItemContextMenuItems.getStatusBarItemContextMenuItems(item.extensionId, item.providerId)
-    : []
+  const contextMenuItems =
+    item?.extensionId && item.providerId
+      ? await GetStatusBarItemContextMenuItems.getStatusBarItemContextMenuItems(item.extensionId, item.providerId)
+      : []
   await ContextMenu.show2(state.uid, MenuEntryId.StatusBar, x, y, {
     contextMenuItems,
     menuId: MenuEntryId.StatusBar,
